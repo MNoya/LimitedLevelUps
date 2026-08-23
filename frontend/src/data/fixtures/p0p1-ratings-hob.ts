302abef34636858cd1d4f20b8979c44d672dcddc
Sean Li <9913851+saifahn@users.noreply.github.com>
2026-08-23 23:48:03 +0900

    Reveal HOB P0P1 midway results
    Add the Hobbit ratings fixture with the midway phase so the contest
    page shows partial standings until scoring closes.

---
+++ frontend/src/data/fixtures/p0p1-ratings-hob.ts
@@ -0,0 +1,612 @@
+import type { RatingsSnapshot } from "../p0p1Results";
+
+export default {
+  "setCode": "HOB",
+  "phase": "midway",
+  "dateRange": {
+    "start": "2026-08-11",
+    "end": "2026-08-23"
+  },
+  "cards": [
+    {
+      "card_name": "Long-Bodied Grey Dog",
+      "gihwr": 0.51769,
+      "gih": 5504
+    },
+    {
+      "card_name": "Old Thrush",
+      "gihwr": 0.52106,
+      "gih": 5802
+    },
+    {
+      "card_name": "Troop of Ponies",
+      "gihwr": 0.53341,
+      "gih": 5866
+    },
+    {
+      "card_name": "Bofur, Reliable Guardian",
+      "gihwr": 0.55698,
+      "gih": 10594
+    },
+    {
+      "card_name": "Celebrate the Mountain-king",
+      "gihwr": 0.61769,
+      "gih": 20306
+    },
+    {
+      "card_name": "Dáin, Lord of the Iron Hills",
+      "gihwr": 0.53981,
+      "gih": 9644
+    },
+    {
+      "card_name": "Dwarven Provisioner",
+      "gihwr": 0.54684,
+      "gih": 22068
+    },
+    {
+      "card_name": "Dwarven Shortsword",
+      "gihwr": 0.53202,
+      "gih": 16751
+    },
+    {
+      "card_name": "Eagle of the Great Shelf",
+      "gihwr": 0.51779,
+      "gih": 5980
+    },
+    {
+      "card_name": "Esgaroth Garrison",
+      "gihwr": 0.55554,
+      "gih": 8513
+    },
+    {
+      "card_name": "Iron Hills Blacksmith",
+      "gihwr": 0.57402,
+      "gih": 14163
+    },
+    {
+      "card_name": "Lake-town Lookout",
+      "gihwr": 0.51941,
+      "gih": 7875
+    },
+    {
+      "card_name": "Lake-town Toymaker",
+      "gihwr": 0.53904,
+      "gih": 7334
+    },
+    {
+      "card_name": "Magnificent End",
+      "gihwr": 0.55859,
+      "gih": 31569
+    },
+    {
+      "card_name": "Moment of Glory",
+      "gihwr": 0.5099,
+      "gih": 5185
+    },
+    {
+      "card_name": "The Mountain-king's Return",
+      "gihwr": 0.60834,
+      "gih": 18685
+    },
+    {
+      "card_name": "Ori, Keeper of Songs",
+      "gihwr": 0.55069,
+      "gih": 14252
+    },
+    {
+      "card_name": "Stone by Sunlight",
+      "gihwr": 0.60197,
+      "gih": 17821
+    },
+    {
+      "card_name": "Thorin's Last Stand",
+      "gihwr": 0.4861,
+      "gih": 3129
+    },
+    {
+      "card_name": "Velvetwing Butterflies",
+      "gihwr": 0.52784,
+      "gih": 9319
+    },
+    {
+      "card_name": "Vow to Erebor",
+      "gihwr": 0.51477,
+      "gih": 4335
+    },
+    {
+      "card_name": "Bilbo, Luckwearer",
+      "gihwr": 0.60003,
+      "gih": 19069
+    },
+    {
+      "card_name": "Bilbo Baggins, Burglar",
+      "gihwr": 0.57152,
+      "gih": 38663
+    },
+    {
+      "card_name": "Confusticate and Bebother",
+      "gihwr": 0.53672,
+      "gih": 14883
+    },
+    {
+      "card_name": "Elven Raft-Steerer",
+      "gihwr": 0.49491,
+      "gih": 2972
+    },
+    {
+      "card_name": "Elvenking's Harper",
+      "gihwr": 0.52464,
+      "gih": 7453
+    },
+    {
+      "card_name": "Enchanted River's Grasp",
+      "gihwr": 0.55137,
+      "gih": 32183
+    },
+    {
+      "card_name": "Gandalf, Wandering Wizard",
+      "gihwr": 0.51257,
+      "gih": 4935
+    },
+    {
+      "card_name": "Lakeshore Apothecary",
+      "gihwr": 0.57358,
+      "gih": 44527
+    },
+    {
+      "card_name": "Lake-town Mariners",
+      "gihwr": 0.51208,
+      "gih": 4096
+    },
+    {
+      "card_name": "Long Lake Nuisance",
+      "gihwr": 0.57701,
+      "gih": 37710
+    },
+    {
+      "card_name": "Master's Councillors",
+      "gihwr": 0.58444,
+      "gih": 19131
+    },
+    {
+      "card_name": "Mirkwood Meditator",
+      "gihwr": 0.47556,
+      "gih": 1249
+    },
+    {
+      "card_name": "Old Fat Spider Can't See Me",
+      "gihwr": 0.5235,
+      "gih": 7787
+    },
+    {
+      "card_name": "Plunder the Trollshaws",
+      "gihwr": 0.58455,
+      "gih": 36888
+    },
+    {
+      "card_name": "Ravenhill Flock",
+      "gihwr": 0.5163,
+      "gih": 6582
+    },
+    {
+      "card_name": "Sound the Trumpets",
+      "gihwr": 0.55048,
+      "gih": 6200
+    },
+    {
+      "card_name": "Thranduil's Decree",
+      "gihwr": 0.50892,
+      "gih": 3566
+    },
+    {
+      "card_name": "Uneasy Partings",
+      "gihwr": 0.55833,
+      "gih": 28471
+    },
+    {
+      "card_name": "Bilbo's Deadly Slice",
+      "gihwr": 0.57144,
+      "gih": 31538
+    },
+    {
+      "card_name": "Crude Bent Blade",
+      "gihwr": 0.60506,
+      "gih": 39581
+    },
+    {
+      "card_name": "Desolation Prowler",
+      "gihwr": 0.6149,
+      "gih": 17944
+    },
+    {
+      "card_name": "Down, Down to Goblin-town",
+      "gihwr": 0.59333,
+      "gih": 16189
+    },
+    {
+      "card_name": "Dreaded Bat-Cloud",
+      "gihwr": 0.56075,
+      "gih": 10876
+    },
+    {
+      "card_name": "Front Porch Sentries",
+      "gihwr": 0.54965,
+      "gih": 17703
+    },
+    {
+      "card_name": "Gathering of Darkness",
+      "gihwr": 0.54754,
+      "gih": 10756
+    },
+    {
+      "card_name": "Gnashing of Teeth",
+      "gihwr": 0.55577,
+      "gih": 12449
+    },
+    {
+      "card_name": "Gollum, Silent Slinker",
+      "gihwr": 0.59297,
+      "gih": 32525
+    },
+    {
+      "card_name": "Gollum the Abandoned",
+      "gihwr": 0.57077,
+      "gih": 11649
+    },
+    {
+      "card_name": "Great Fierce Bee",
+      "gihwr": 0.52325,
+      "gih": 6312
+    },
+    {
+      "card_name": "Great Ugly-Looking Goblin",
+      "gihwr": 0.57143,
+      "gih": 12650
+    },
+    {
+      "card_name": "Nighthowl Pursuer",
+      "gihwr": 0.5912,
+      "gih": 13444
+    },
+    {
+      "card_name": "Rage into the Valley",
+      "gihwr": 0.55698,
+      "gih": 29865
+    },
+    {
+      "card_name": "Ravening Warg",
+      "gihwr": 0.57376,
+      "gih": 28543
+    },
+    {
+      "card_name": "Reverent Howl",
+      "gihwr": 0.532,
+      "gih": 10892
+    },
+    {
+      "card_name": "Stir Up Trouble",
+      "gihwr": 0.56207,
+      "gih": 31784
+    },
+    {
+      "card_name": "Stony-Voiced Goblins",
+      "gihwr": 0.55551,
+      "gih": 27813
+    },
+    {
+      "card_name": "Bombur, Gentle Dreamer",
+      "gihwr": 0.54457,
+      "gih": 8131
+    },
+    {
+      "card_name": "Bothersome Noisemaker",
+      "gihwr": 0.60882,
+      "gih": 17436
+    },
+    {
+      "card_name": "Burn, Burn, Tree and Fern",
+      "gihwr": 0.59705,
+      "gih": 15720
+    },
+    {
+      "card_name": "Dori, Bearer of Friends",
+      "gihwr": 0.56815,
+      "gih": 27278
+    },
+    {
+      "card_name": "Dwarven Mauler",
+      "gihwr": 0.56728,
+      "gih": 11196
+    },
+    {
+      "card_name": "Gandalf, Spark Starter",
+      "gihwr": 0.58734,
+      "gih": 13454
+    },
+    {
+      "card_name": "Glóin the Mighty",
+      "gihwr": 0.57742,
+      "gih": 10913
+    },
+    {
+      "card_name": "Goblin-town Flunkies",
+      "gihwr": 0.53353,
+      "gih": 16094
+    },
+    {
+      "card_name": "Gundabad Opportunist",
+      "gihwr": 0.5557,
+      "gih": 24265
+    },
+    {
+      "card_name": "Iron Hills Stalwart",
+      "gihwr": 0.5131,
+      "gih": 6445
+    },
+    {
+      "card_name": "Misty Mountains Raider",
+      "gihwr": 0.57748,
+      "gih": 15011
+    },
+    {
+      "card_name": "Óin the Brave",
+      "gihwr": 0.54903,
+      "gih": 18221
+    },
+    {
+      "card_name": "Pinecone Strike",
+      "gihwr": 0.58979,
+      "gih": 37662
+    },
+    {
+      "card_name": "Ragged Short Spear",
+      "gihwr": 0.54717,
+      "gih": 15605
+    },
+    {
+      "card_name": "Smaug, the Great Calamity",
+      "gihwr": 0.55588,
+      "gih": 20365
+    },
+    {
+      "card_name": "Smaug's Fury",
+      "gihwr": 0.5181,
+      "gih": 4341
+    },
+    {
+      "card_name": "Snowslope Hunter",
+      "gihwr": 0.53261,
+      "gih": 9607
+    },
+    {
+      "card_name": "Tidings of War",
+      "gihwr": 0.55257,
+      "gih": 21154
+    },
+    {
+      "card_name": "Attercop",
+      "gihwr": 0.55298,
+      "gih": 19651
+    },
+    {
+      "card_name": "Beorn, Reluctant Host",
+      "gihwr": 0.50186,
+      "gih": 6941
+    },
+    {
+      "card_name": "Beorn's Hospitality",
+      "gihwr": 0.54171,
+      "gih": 6009
+    },
+    {
+      "card_name": "Boughside Wanderers",
+      "gihwr": 0.52909,
+      "gih": 9746
+    },
+    {
+      "card_name": "Galion, Elvenking's Butler",
+      "gihwr": 0.53167,
+      "gih": 5316
+    },
+    {
+      "card_name": "Guardian of the Halls",
+      "gihwr": 0.52327,
+      "gih": 10994
+    },
+    {
+      "card_name": "Little Bear",
+      "gihwr": 0.50914,
+      "gih": 5487
+    },
+    {
+      "card_name": "Mirkwood Pathmaker",
+      "gihwr": 0.59654,
+      "gih": 12525
+    },
+    {
+      "card_name": "Nasty Little Rabbit",
+      "gihwr": 0.58544,
+      "gih": 10606
+    },
+    {
+      "card_name": "Old Fat Spider",
+      "gihwr": 0.52755,
+      "gih": 6983
+    },
+    {
+      "card_name": "Ordinary Bear",
+      "gihwr": 0.54375,
+      "gih": 9934
+    },
+    {
+      "card_name": "Quarrel",
+      "gihwr": 0.56057,
+      "gih": 24110
+    },
+    {
+      "card_name": "Troll Negotiations",
+      "gihwr": 0.54779,
+      "gih": 9675
+    },
+    {
+      "card_name": "Warg Tactics",
+      "gihwr": 0.53501,
+      "gih": 8623
+    },
+    {
+      "card_name": "Wargling",
+      "gihwr": 0.53854,
+      "gih": 13264
+    },
+    {
+      "card_name": "Wilderland Scrounger",
+      "gihwr": 0.51378,
+      "gih": 5904
+    },
+    {
+      "card_name": "Wood Elves",
+      "gihwr": 0.53274,
+      "gih": 12534
+    },
+    {
+      "card_name": "Woodland Weavemaster",
+      "gihwr": 0.5426,
+      "gih": 5177
+    },
+    {
+      "card_name": "Bard the Bowman",
+      "gihwr": 0.58221,
+      "gih": 17037
+    },
+    {
+      "card_name": "Bifur, Melodic Rider",
+      "gihwr": 0.5424,
+      "gih": 7746
+    },
+    {
+      "card_name": "Bolg of the North",
+      "gihwr": 0.54467,
+      "gih": 9498
+    },
+    {
+      "card_name": "The Chief Warg",
+      "gihwr": 0.5264,
+      "gih": 8016
+    },
+    {
+      "card_name": "Duskwatch Hunter",
+      "gihwr": 0.55598,
+      "gih": 20535
+    },
+    {
+      "card_name": "Eagle's Rescue",
+      "gihwr": 0.55626,
+      "gih": 9458
+    },
+    {
+      "card_name": "Fearsome Goblin Pair",
+      "gihwr": 0.5352,
+      "gih": 9449
+    },
+    {
+      "card_name": "Goblin Plate Mail",
+      "gihwr": 0.56746,
+      "gih": 38292
+    },
+    {
+      "card_name": "Large Bear",
+      "gihwr": 0.56592,
+      "gih": 11431
+    },
+    {
+      "card_name": "Mirkwood Nurturer",
+      "gihwr": 0.54784,
+      "gih": 19938
+    },
+    {
+      "card_name": "Nori, Teller of Tales",
+      "gihwr": 0.53465,
+      "gih": 17261
+    },
+    {
+      "card_name": "Patient Instructor",
+      "gihwr": 0.5808,
+      "gih": 41035
+    },
+    {
+      "card_name": "Silvan Reveler",
+      "gihwr": 0.53435,
+      "gih": 7290
+    },
+    {
+      "card_name": "Thorin Oakenshield",
+      "gihwr": 0.55598,
+      "gih": 9396
+    },
+    {
+      "card_name": "Thranduil, Sindarin Liege",
+      "gihwr": 0.57787,
+      "gih": 11592
+    },
+    {
+      "card_name": "The Black Arrow",
+      "gihwr": 0.51036,
+      "gih": 3077
+    },
+    {
+      "card_name": "Dwarven Mattock",
+      "gihwr": 0.55425,
+      "gih": 7911
+    },
+    {
+      "card_name": "Giant's Boulder",
+      "gihwr": 0.49236,
+      "gih": 4968
+    },
+    {
+      "card_name": "Key to the Side-Door",
+      "gihwr": 0.47628,
+      "gih": 936
+    },
+    {
+      "card_name": "Thrór's Map",
+      "gihwr": 0.54037,
+      "gih": 9346
+    },
+    {
+      "card_name": "Well-Worn Spatula",
+      "gihwr": 0.5277,
+      "gih": 6138
+    },
+    {
+      "card_name": "Elvenking's Halls",
+      "gihwr": 0.52815,
+      "gih": 10464
+    },
+    {
+      "card_name": "Goblin-town",
+      "gihwr": 0.55098,
+      "gih": 18323
+    },
+    {
+      "card_name": "Hobbit Hole",
+      "gihwr": 0.55426,
+      "gih": 35840
+    },
+    {
+      "card_name": "Iron Hills",
+      "gihwr": 0.53815,
+      "gih": 14681
+    },
+    {
+      "card_name": "Lake-town",
+      "gihwr": 0.55538,
+      "gih": 21056
+    },
+    {
+      "card_name": "Mirkwood",
+      "gihwr": 0.55076,
+      "gih": 14418
+    }
+  ]
+} satisfies RatingsSnapshot;
